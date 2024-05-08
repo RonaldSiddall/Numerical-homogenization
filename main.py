829566535793ef28f5d03e63c3d5e7019e896c4b
RonaldSiddall <114618003+RonaldSiddall@users.noreply.github.com>
2024-05-08 16:24:29 +0200

    Fixed and updated user interface

--- main.py
+++ main.py
@@ -5,36 +5,36 @@ from ConfigManager import ConfigManager
 from GenerateMesh import GenerateMesh
 
 
+# main is used as a one of computation of mechanical properties based on config_file.yaml
 def main(config_file):
     try:
         file_msh = GenerateMesh(config_file).generate_mesh_based_on_sample()
         time_decider = ConfigManager(config_file).get_measure_time_of_computation()
         if time_decider == "yes":
+            start_time = time.time()
+            EffectiveElasticTensor(file_msh, config_file).get_tensor_in_txt_formatted()
             print("\n=========================================================================\n")
             print("         The effective elastic tensor was successfully computed!")
             print("\n=========================================================================\n")
             print("Additional information about the simulation process:")
             print("------------------------------------------------------")
             print("  - The results were saved in a file named '"
-                + ConfigManager(config_file).get_name_of_file_with_tensor()
-                + ".txt' that was created in this directory:")
+                  + ConfigManager(config_file).get_name_of_file_with_tensor()
+                  + ".txt' that was created in this directory:")
             print("    " + ConfigManager(config_file).get_output_dir_of_file_with_tensor())
-
-            start_time = time.time()
-            EffectiveElasticTensor(file_msh, config_file).get_tensor_in_txt_formatted()
             end_time = time.time()
             print("\n  - Time it took to do the computation:", end_time - start_time, "seconds")
         else:
+            EffectiveElasticTensor(file_msh, config_file).get_tensor_in_txt_formatted()
             print("\n=========================================================================\n")
             print("         The effective elastic tensor was successfully computed!")
             print("\n=========================================================================\n")
             print("Additional information about the simulation process:")
             print("------------------------------------------------------")
             print("  - The results were saved in a file named '"
-                + ConfigManager(config_file).get_name_of_file_with_tensor()
-                + ".txt' that was created in this directory:")
+                  + ConfigManager(config_file).get_name_of_file_with_tensor()
+                  + ".txt' that was created in this directory:")
             print("    " + ConfigManager(config_file).get_output_dir_of_file_with_tensor())
-            EffectiveElasticTensor(file_msh, config_file).get_tensor_in_txt_formatted()
     except Exception as error_mess:
         # If any error occurs during execution, print the error message
         print("---------------------------------------------------------------------------")
@@ -51,7 +51,7 @@ if __name__ == "__main__":
             print("-------------------------------------------------------------------------")
             print("\nAn error occurred during execution.")
             print("Please make sure to enter the command like this:\n")
-            print("python <absolute_path_to_main.py> <absolute_path_to_config_file.yaml>\n")
+            print("python <path_to_main.py> <path_to_config_file.yaml>\n")
             print("-------------------------------------------------------------------------")
             # Exit the script with an error status code
             sys.exit(1)
@@ -63,7 +63,7 @@ if __name__ == "__main__":
         main(config_file)
 
     except Exception as error_message:
-        # If any error occurs during execution, print the error message
+        # If any error occurs during execution, prints the error message
         print("----------------------------------------------------------------------------")
         print("\nAn error occurred during execution. The error message is written below:\n")
         print(str(error_message)+"\n")
