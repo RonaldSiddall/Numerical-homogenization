abf13ba5442f16b9cc0a170902be981f4ce6818d
RonaldSiddall <114618003+RonaldSiddall@users.noreply.github.com>
2024-05-08 16:20:41 +0200

    Removed non working visualisation

--- ConfigManager.py
+++ ConfigManager.py
@@ -30,43 +30,43 @@ class ConfigManager:
         return self.config_data["simulation_parameters"]["lc_parameter"]
 
     def get_E1(self):
-        return self.config_data["simulation_parameters"]["E1"]
+        # returns the load matrix E1 with load parameter = 1
+        return [[1, 0], [0, 0]]
 
     def get_E2(self):
-        return self.config_data["simulation_parameters"]["E2"]
+        # returns the load matrix E2 with load parameter = 1
+        return [[0, 0], [0, 1]]
 
     def get_E3(self):
-        return self.config_data["simulation_parameters"]["E3"]
+        # returns the load matrix E3 with load parameter = 1
+        return [[0, 1], [1, 0]]
+
+    def get_absolute_path_to_dir_with_data(self):
+        return self.config_data["directories"]["absolute_path_to_dir_with_data"]
 
     def get_dir_where_yamls_are_created(self):
-        return self.config_data["directories"]["dir_where_yamls_are_created"]
+        return self.get_absolute_path_to_dir_with_data() + self.config_data["directories"]["dir_where_yamls_are_created"]
 
     def get_absolute_path_to_yaml_template(self):
-        return self.config_data["directories"]["absolute_path_to_yaml_template"]
+        return self.get_absolute_path_to_dir_with_data() + self.config_data["directories"]["absolute_path_to_yaml_template"]
 
     def get_directory_where_vtus_are_created(self):
-        return self.config_data["directories"]["directory_where_vtus_are_created"]
+        return self.get_absolute_path_to_dir_with_data() + self.config_data["directories"]["directory_where_vtus_are_created"]
 
     def dir_where_mesh_and_geo_are_created(self):
-        return self.config_data["directories"]["dir_where_mesh_and_geo_are_created"]
+        return self.get_absolute_path_to_dir_with_data() + self.config_data["directories"]["dir_where_mesh_and_geo_are_created"]
 
     def get_name_of_file_with_tensor(self):
         return self.config_data["results_file_settings"]["name_of_file_with_tensor"]
 
     def get_output_dir_of_file_with_tensor(self):
-        return self.config_data["results_file_settings"]["output_dir_of_file_with_tensor"]
+        return self.get_absolute_path_to_dir_with_data() + self.config_data["results_file_settings"]["output_dir_of_file_with_tensor"]
 
     def get_delete_yaml_dir_after_simulation(self):
         return self.config_data["additional_settings"]["delete_yaml_dir_after_simulation"]
 
     def get_delete_vtu_dir_after_simulation(self):
         return self.config_data["additional_settings"]["delete_vtu_dir_after_simulation"]
-
-    def get_want_to_display_visualisation(self):
-        return self.config_data["additional_settings"]["want_to_display_visualisation"]
-
-    def get_what_to_display(self):
-        return self.config_data["additional_settings"]["what_to_display"]
 
     def get_change_names_of_computed_yamls(self):
         return self.config_data["additional_settings"]["change_names_of_computed_yamls"]
